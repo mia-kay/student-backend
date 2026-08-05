9c1394be63105a4ae7fcdd4430e120bd19798c7b
Mia <ewurama1894@gmail.com>
2026-08-05 17:47:15 +0100

    create student backend

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const Student = require("./models/Student");
+
+
+
+
+const server = express();
+server.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("MongoDB connection error:", error);
+    });
+
+    server.use(express.json());
+
+
+
+
+    server.post("/students", async (req, res) => {
+    try {
+        const student = await Student.create(req.body);
+        res.status(201).json(student);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+
+
+
+server.get("/students", async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.status(200).json(students);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
